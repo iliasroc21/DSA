d396883364373e9dfc47587bc1f21d0c8023fe4b
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-08-17 12:17:58 +0100

    exercices

---
+++ src/leetcode/array/MaxProfit.java
@@ -0,0 +1,15 @@
+package leetcode.array;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            int profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+
+}
---
+++ src/leetcode/array/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+package leetcode.array;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int index = 1 ;
+        for(int i = 1 ; i < nums.length  ; i++){
+            if(nums[i] !=nums[i-1]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
