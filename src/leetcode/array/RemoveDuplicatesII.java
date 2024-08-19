1786861299c213a8ae34334649ed24de5dd39e76
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-08-19 11:43:21 +0100

    Remove Duplicates II 80

---
+++ src/leetcode/array/RemoveDuplicatesII.java
@@ -0,0 +1,14 @@
+package leetcode.array;
+
+public class RemoveDuplicatesII {
+    public int removeDuplicates(int[] nums) {
+        int index = 2 ;
+        for(int i = 2 ; i < nums.length ;i++){
+            if(nums[i]!=nums[index-2]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
