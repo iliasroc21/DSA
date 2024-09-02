4f499136298388a37d814f86ee96248c508bdce8
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-09-02 13:25:18 +0100

    155. Min Stack

---
+++ src/leetcode/stack_and_queue/MinStack.java
@@ -0,0 +1,31 @@
+package leetcode.stack_and_queue;
+
+import java.util.Stack;
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<>();
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        if(val <=min){
+            stack.push(min);
+            min = val ;
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if(stack.pop()==min)min = stack.pop();
+
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
