76cfaadcdcf0739a8d9435330ef64f44296ce517
iliasroc21 <ilyassrouchdi7@gmail.com>
2024-09-19 11:48:11 +0100

    242. Valid Anagram

---
+++ src/leetcode/hash_tables/ValidAnagram.java
@@ -0,0 +1,17 @@
+package leetcode.hash_tables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        Map<Character, Integer> mapS = new HashMap<>();
+        Map<Character, Integer> mapT = new HashMap<>();
+        for(int i = 0; i <s.length();i++){
+            mapS.put(s.charAt(i) , mapS.getOrDefault(s.charAt(i),0)+1);
+            mapT.put(t.charAt(i) , mapT.getOrDefault(t.charAt(i),0)+1);
+        }
+        return mapS.equals(mapT);
+    }
+}
